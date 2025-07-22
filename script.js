f56c801c213a9951f6299792aac37ec567a3593a
Thomas <24tk@suffieldacademy.org>
2025-07-22 13:24:14 -0400

    status, winning line

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const squares = document.querySelectorAll('.square');
 //set background
 document.body.style.backgroundColor = "#1fe0aa";
 
+//if game is complete 
+//  makes it so you cant comtinue to play after someone wins
+let gameOver = false;
+
 let boxList = Array(9).fill(null);
+let objectList = Array(9).fill(null);
 
 const winPatterns = [
     [0, 1, 2],
@@ -33,10 +38,10 @@ squares.forEach(square => {
         const id = event.target.id;
         const parsedId = parseInt(id);
 
-        
 
         //if the thing hasnt been clicked
-        if (event.target.dataset.clicked == "false") {
+        if (event.target.dataset.clicked == "false" && gameOver == false) {
+            
             if(currentPlayer == 1) {
                 event.target.textContent = "O";
                 event.target.style.color = "blue";
@@ -44,9 +49,11 @@ squares.forEach(square => {
                 document.body.style.backgroundColor = "#f54245";
                 currentPlayer = 2;
 
-                boxList[parsedId] = "1";
+                boxList[parsedId] = "O";
+                objectList[parsedId] = event.target;
 
                 moveCount++;
+                updateStatus("Its X's turn");
             } else {
                 event.target.textContent = "X";
                 event.target.style.color = "Red";
@@ -54,9 +61,13 @@ squares.forEach(square => {
                 document.body.style.backgroundColor = "#42b3f5";
                 currentPlayer = 1;
 
-                boxList[parsedId] = "2";
+                boxList[parsedId] = "X";
+                objectList[parsedId] = event.target;
+
 
                 moveCount++;
+                updateStatus("Its O's turn");
+
             }
         }
 
@@ -72,27 +83,40 @@ function checkWinner(squares) {
          let box1 = boxList[winPat[0]];
          let box2 = boxList[winPat[1]];
          let box3 = boxList[winPat[2]];
+         let object1 = objectList[winPat[0]];
+         let object2 = objectList[winPat[1]];
+         let object3 = objectList[winPat[2]];
+
 
          //make sure all of the squares are filled
         if(box1 !== null && box2 !== null && box3 !== null) {
 
             //if they are equal to each other
             if(box1 == box2 && box2 == box3) {
+                object1.style.backgroundColor = "#02e34d";
+                object2.style.backgroundColor = "#02e34d";
+                object3.style.backgroundColor = "#02e34d";
+
                 console.log("winner");
+                let winner = box1;
+                let winMessage = `${winner} Wins!`;
+                updateStatus(winMessage);
+                gameOver = true;       
             }
         }
-
     }
-    
-    
 };
 
+//resets the game back to its origianl state
 function reset() {
     boxList = Array(9).fill(null);
+    objectList = Array(9).fill(null);
     document.body.style.backgroundColor = "#1fe0aa";
-
+    gameOver = false;
+    updateStatus("Its O's turn");
 
     squares.forEach(square => {
+        square.style.backgroundColor = "#FFFFFF";
         square.textContent = "";
         square.dataset.clicked = "false";
 
@@ -108,3 +132,9 @@ resetButton.addEventListener('click', function (event) {
 });
 
 
+function updateStatus(newtext) {
+
+    const status = document.getElementById("Status");
+    status.textContent = newtext;
+
+}
